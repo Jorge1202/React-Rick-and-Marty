bcf9191a0d7cd88cc6e84e2bad888c752fd52c73
Jorge1202 <yorch.uaeh@gmail.com>
2022-01-05 14:58:39 -0600

    first commit

---
+++ src/routes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/global.scss'
+
+const App = () => {
+    return (
+        <div >
+            Esto es una prueba
+        </div>
+    );
+};
+
+export default App;
