0f343783a82928ccb12f1b0bfd63a96577f727be
Jorge1202 <yorch.uaeh@gmail.com>
2024-02-08 09:10:48 -0600

    add button start

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 // import fondo from '../assets/media/img/error-404.jpg' 
-import fondo from'@img/error-404.jpg' 
+import fondo from '@img/error-404.jpg'
+import { Link } from "react-router-dom";
+
 import './styles/NotFound.scss'
 
 const NotFound = () => {
     return (
         <div className='NotFound'>
-            <div className='conttenido' style={{ backgroundImage: `url(${fondo})`}}>
+            <Link to="/" className='btn btn-light'>Ir al inicio</Link>
+            <div className='conttenido' style={{ backgroundImage: `url(${fondo})` }}>
             </div>
+
         </div>
     );
 };
