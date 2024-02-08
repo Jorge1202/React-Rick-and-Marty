18697c5c651a4381c8b2492097a9a900c7eee70e
Jorge1202 <yorch.uaeh@gmail.com>
2024-02-08 09:21:34 -0600

    change url

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -12,7 +12,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/React-Rick-and-Marty/" element={<Home />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
